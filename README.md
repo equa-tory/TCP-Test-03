1a9c00f68490c914a936aa6315b20c1b5d599686
Equa <TheSuspect9980@gmail.com>
2025-03-05 22:17:41 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TCP-Test-03
+ TCP/UDP test project
---
+++ Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Server;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
